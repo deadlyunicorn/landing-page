738a8ae1b90a9a38728fd5b36004abf9b534d7fa
deadlyunicorn <retroalex1008@gmail.com>
2023-10-17 13:52:02 +0300

    +updated section scroller to use paths (better for Developer Sanity and React)

--- app/lib/SectionScroller.tsx
+++ app/lib/SectionScroller.tsx
@@ -1,94 +1,94 @@
 "use client"
 
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react"
 
 
 export const SectionScroller = () => {
 
   const sections = [
-    "#table_of_contents",
-    "#about_section",
-    "#portfolio_section",
-    "#contact_section"
+    "/",
+    "/about",
+    "/portfolio",
+    "/contact"
   ];
-  const [ currentSection, setCurrentSection ] = useState('#table_of_contents');
+  const router = useRouter();
+
+
+  const currentSection = usePathname();
+
 
   const [ loading, setLoading ] = useState( false );
+
+  const handlePageChange = ( nextPageRequest:boolean ) => {
+    const currentIndex = sections.indexOf( currentSection );
+        const sectionLength = sections.length;
+  
+        if ( !loading ){
+          setLoading( true );
+  
+  
+          const newSection = ( `${sections[ 
+            nextPageRequest
+            ? currentIndex > 0 
+              ? currentIndex - 1
+              : currentIndex 
+            : currentIndex < sectionLength - 1
+              ? currentIndex + 1
+              : currentIndex ]}`
+          )
+  
+          if ( newSection !== currentSection ){
+            router.replace( newSection );
+          }
+  
+          setTimeout( ()=> {
+            setLoading(false);
+          }, 200)
+        }
+  }
 
   useEffect( ()=>{
 
     onwheel = ( wheelEvent ) => {
-      
-      const currentIndex = sections.indexOf( currentSection );
-      const sectionLength = sections.length;
-
-      if ( !loading ){
-        setLoading( true );
-
-
-        setCurrentSection ( sections[ 
-          wheelEvent.deltaY < 0 
-          ? currentIndex > 0 
-            ? currentIndex - 1
-            : currentIndex 
-          : currentIndex < sectionLength - 1
-            ? currentIndex + 1
-            : currentIndex ]
-        )
-
-        console.log( currentSection );
-
-
-        setTimeout( ()=> {
-          setLoading(false);
-        }, 200)
-
-      }
-      
-
+      handlePageChange( wheelEvent.deltaY < 0 ); 
     }})
 
-      /*
-      if ( "ontouchstart" in window){
+    if ( "ontouchstart" in window ){
 
-        ontouchmove = ( touchMoveEvent ) => {
-          console.log( touchMoveEvent );
-        }
-        ontouchend = ( touchend ) => {
-          alert( " end" )
-        } 
+      let touchInitial = 0;
+
+      ontouchstart = ( touchStartEvent ) => {
+        touchInitial = ( touchStartEvent.touches[0].pageY )
       }
-      
-      
-      console.log(currentSection)
-      */
+      ontouchmove = ( touchMoveEvent ) => {
+        if ( touchMoveEvent.touches[0].pageY < touchInitial ){
+          handlePageChange( false );
+        }
+        else{
+          handlePageChange( true );
+        }
+      }
 
-  useEffect( ()=> {
-
-    document.location.hash = currentSection;
-
-  } )
+    }
 
 
   return ( 
     <aside className="fixed right-2 top-[40vh]">
       <ul className="flex flex-col gap-y-2">
         {
-          sections.map( section => {
-            return (
-              <a 
-                onClick={ ( e )=> {
-                  setCurrentSection( section )
-                }}
-                href={section}>
+          sections.map( section => (
+              <Link
+                replace={true}
+                href={`${section}`}>
                 <li  
-                  data-currentSection={ section == currentSection }
+                  data-current-section={ `${section}` == currentSection }
                   className="
-                    bg-slate-200 data-[currentSection=true]:bg-amber-400
+                    bg-slate-200 data-[current-section=true]:bg-amber-400
                     rounded-full w-2 h-2 border border-slate-200"/>
-              </a>
-            )
-          })
+              </Link>
+            ))
         }
       </ul>
     </aside>
@@ -96,3 +96,4 @@ export const SectionScroller = () => {
 }
 
 const delay = async( ms: number ) => new Promise ( res => { setTimeout( res, ms)});
+
