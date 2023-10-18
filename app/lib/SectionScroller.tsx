d2bd7161ab25d0ad2ddd2b4bdadeb6079834bade
deadlyunicorn <retroalex1008@gmail.com>
2023-10-18 03:57:39 +0300

    + more navigation options

--- app/lib/SectionScroller.tsx
+++ app/lib/SectionScroller.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react"
+import { BlurSpan } from "./BlurSpan";
 
 
 export const SectionScroller = () => {
@@ -20,9 +21,12 @@ export const SectionScroller = () => {
 
 
   const [ loading, setLoading ] = useState( false );
+  const [ hasMounted, setHasMounted ] = useState( false );
+  const currentSectionsIndex = sections.indexOf( currentSection );
+
 
   const handlePageChange = ( nextPageRequest:boolean ) => {
-    const currentIndex = sections.indexOf( currentSection );
+    const currentIndex = currentSectionsIndex;
         const sectionLength = sections.length;
   
         if ( !loading ){
@@ -51,47 +55,109 @@ export const SectionScroller = () => {
 
   useEffect( ()=>{
 
-    onwheel = ( wheelEvent ) => {
-      handlePageChange( wheelEvent.deltaY < 0 ); 
-    }})
+    if ( hasMounted ){
 
-    if ( "ontouchstart" in window ){
-
-      let touchInitial = 0;
-
-      ontouchstart = ( touchStartEvent ) => {
-        touchInitial = ( touchStartEvent.touches[0].pageY )
+      onwheel = ( wheelEvent ) => {
+        handlePageChange( wheelEvent.deltaY < 0 ); 
       }
-      ontouchmove = ( touchMoveEvent ) => {
-        if ( touchMoveEvent.touches[0].pageY < touchInitial ){
-          handlePageChange( false );
+  
+      if ( window && "ontouchstart" in window ){
+  
+        let touchInitial = 0;
+  
+        ontouchstart = ( touchStartEvent ) => {
+          touchInitial = ( touchStartEvent.touches[0].pageY )
         }
-        else{
-          handlePageChange( true );
+        ontouchmove = ( touchMoveEvent ) => {
+          if ( touchMoveEvent.touches[0].pageY < touchInitial ){
+            handlePageChange( false );
+          }
+          else{
+            handlePageChange( true );
+          }
         }
+  
       }
 
     }
+    else{
+      setHasMounted( true );
+    }})
 
+  const nextSection = sections [ currentSectionsIndex + 1 ];
+  const prevSection = sections [ currentSectionsIndex - 1 ];
+    
 
   return ( 
+    <>
     <aside className="fixed right-2 top-[40vh]">
       <ul className="flex flex-col gap-y-2">
         {
-          sections.map( section => (
+          sections.map( ( section, key ) => (
               <Link
+                tabIndex={0}
+                className="group outline-none"
+                key={ `${section}_${key}` }
                 replace={true}
                 href={`${section}`}>
                 <li  
                   data-current-section={ `${section}` == currentSection }
                   className="
-                    bg-slate-200 data-[current-section=true]:bg-amber-400
+                  bg-slate-200 
+                  group-focus:bg-amber-600
+                  group-hover:bg-blue-600
+                    data-[current-section=true]:bg-amber-400
                     rounded-full w-2 h-2 border border-slate-200"/>
               </Link>
             ))
         }
       </ul>
     </aside>
+    
+    <aside 
+    className="
+      fixed
+      bottom-0 
+      flex w-full 
+      justify-between 
+      text-3xl md:text-5xl
+      px-4 pb-2">
+    { prevSection ?
+      <Link
+        className="relative group outline-none overflow-visible"
+        tabIndex={ 0 } 
+        href={ prevSection }>
+        <BlurSpan> &lt; </BlurSpan>
+        &lt;
+      </Link>
+      : <div></div>
+    }
+    
+    {  
+    nextSection ?
+      
+      currentSectionsIndex === -1?
+      <div className="w-full flex justify-center text-lg md:text-3xl">
+      <Link
+        tabIndex={0} 
+        className="group outline-none overflow-visible relative"
+        href={ nextSection }>
+        <BlurSpan> Homepage </BlurSpan>
+        Homepage
+      </Link>
+      </div>
+      :
+      <Link
+        tabIndex={0} 
+        className="relative group outline-none overflow-visible"
+        href={ nextSection }>
+        <BlurSpan> &gt; </BlurSpan>
+        &gt;
+      </Link>
+      : <div></div>
+    }
+    </aside>
+    </>
   )
 }
 
