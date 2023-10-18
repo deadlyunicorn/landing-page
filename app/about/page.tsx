53cf1b69668e1c2d84c47927e12aed6e8a298313
deadlyunicorn <retroalex1008@gmail.com>
2023-10-18 03:59:00 +0300

    About me page initial complete commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,31 @@
-const AboutSection = () => (
+import { SectionHeader } from "../lib/SectionHeader";
+import { PageSection } from "../lib/SectionOutline";
+import { mongoClient } from "../api/mongodb/mongoClient";
 
-  <section
+const AboutSection = async() => {
+
+
+  const description = await mongoClient.db('landing_page').collection('sections').findOne( { section: "about" } ).then( res => res? res.content :null );
+  return(
+  <PageSection
     id="about_section">
-    <h3>This is the about section</h3>
-  </section>
-)
+
+      <SectionHeader>
+        <h1> About Me </h1>
+      </SectionHeader>
+
+
+    <div className="h-full mb-[10vh] flex items-center justify-center md:text-2xl">
+      <p
+        dangerouslySetInnerHTML={
+          {__html: description}
+        } 
+        className="text-center"/>
+    </div>
+
+    
+  </PageSection>
+  )
+}
 
 export default AboutSection;
