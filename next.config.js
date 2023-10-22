7fb77a2fa7c807bcd4a5f766c7364e970d6b2b0b
deadlyunicorn <retroalex1008@gmail.com>
2023-10-23 00:44:20 +0300

    +github images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    domains:["deadlyunicorn.s3.eu-central-1.amazonaws.com","lh3.googleusercontent.com"]
+    domains:["deadlyunicorn.s3.eu-central-1.amazonaws.com","lh3.googleusercontent.com","user-images.githubusercontent.com"]
   }
 }
 
