b34502cdf02d6c7b29c6e645e62a441601d0cff0
deadlyunicorn <retroalex1008@gmail.com>
2023-10-22 23:55:20 +0300

    updated nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images:{
+    domains:["deadlyunicorn.s3.eu-central-1.amazonaws.com","lh3.googleusercontent.com"]
+  }
+}
 
 module.exports = nextConfig
