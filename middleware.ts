efb0fda798687be501e65281176eb4b4687a9b84
deadlyunicorn <retroalex1008@gmail.com>
2023-11-30 13:00:19 +0200

    updated linked in link

--- middleware.ts
+++ middleware.ts
@@ -6,16 +6,16 @@ const middleware = async( request: NextRequest ) => {
   switch ( request.nextUrl.pathname.toLowerCase() ){
 
     case "/github":
-      return NextResponse.redirect('https://github.com/deadlyunicorn');
+      return NextResponse.redirect('https://github.com/deadlyunicorn/');
     case "/linkedin":
-      return NextResponse.redirect('https://linkedin.com/in/deadlyunicorn');
+      return NextResponse.redirect('https://www.linkedin.com/in/alexander-petrache/');
     case "/x":
-      return NextResponse.redirect('https://twitter.com/deadlyunicorn_');
+      return NextResponse.redirect('https://twitter.com/deadlyunicorn_/');
     case "/links":
-      return NextResponse.redirect('https://linktr.ee/deadlyunicorn');
+      return NextResponse.redirect('https://linktr.ee/deadlyunicorn/');
     case "/resume":
     case "/cv":
-      return NextResponse.redirect('https://cv-job-resume.vercel.app');
+      return NextResponse.redirect('https://cv-job-resume.vercel.app/');
 
 
   }
